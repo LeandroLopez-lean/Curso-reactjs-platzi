5eea399a498f7d459cdbf864b78cf76402f28717
Leandro <leandrolopez131982@gmail.com>
2023-08-12 15:28:55 -0300

    agregado css (no funcionando)

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import { TodoSearch } from './TodoSearch';
 import { CreateTodoButton } from './CreateButton';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodeItem';
+import React from 'react';
 import './App.css';
+
+const defaultTodos = [
+  { text: "cortar cebolla", completed: true },
+  { text: "tomar curso react", completed: false },
+  { text: "llorar con la llorona", completed: false },
+  { text: "hacer lo que quiera", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
       <TodoCounter  />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
       <CreateTodoButton />
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,3 +1,5 @@
+import './Todocounter.css';
+
 function TodoCounter({total, completed}){
 
     return(
